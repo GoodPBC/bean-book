9eb19a251362ebacd6431fef7c460c32ff2cf2aa
good-aii <aii@goodinc.co>
2023-02-01 16:28:02 -0500

    global footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import "../styles/globals.css";
+import Image from "next/image";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer className="footer">
+        <a
+            className="footer-company"
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Powered by Good Inc. {'  '}
+            <span className="footer-logo-wrapper">
+                <Image src="/GOOD.png" alt="Good Inc Logo" width={32} height={32} />
+            </span>
+        </a>
+      </footer>
+    </div>
+    
+  )
+
 }
 
 export default MyApp;
